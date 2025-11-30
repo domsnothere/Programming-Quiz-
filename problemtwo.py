acfbd7002ae7d94ff937617f8697fe65ba39d3a1
domsnothere <harden.dominique0816@gmail.com>
2025-11-30 07:47:53 +0000

    add and subtract code

--- problemtwo.py
+++ problemtwo.py
@@ -1,1 +1,12 @@
-
+print("Do you want to add or subtract? a/s")
+choice = input()
+first_number = input ("Pick your First Number")
+second_number = input ("Pick your Second Number")
+int_first_number = int(first_number)
+int_second_number = int(second_number)
+if choice == "a":
+    solution = (int_first_number + int_second_number)
+    print ("The solution to the addition problem is: ",solution)
+else:
+    solution = (int_first_number - int_second_number)
+    print ("The solution to the subtraction problem is:",solution) 
